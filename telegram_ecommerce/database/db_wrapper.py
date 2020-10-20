0ec455c2513745d76a2f792d2a243d943051d200
Raul <raulrufino0@gmail.com>
2020-10-20 08:25:17 -0300

    adding function to create tables

--- telegram_ecommerce/database/db_wrapper.py
+++ telegram_ecommerce/database/db_wrapper.py
@@ -1,22 +1,29 @@
 
 import mysql.connector as connector
 
-from ..utils.utils import load_sql_file
+from ..utils.utils import get_sql_commands_from_a_file
 from ..utils.consts import db_credentials
 
 
 class DBWrapper():
     def __init__(self, db_credentials):
-        self.database_name = "ecommerce"
+        self.database_name = "telegram_ecommerce"
         self.connection = connector.connect(**db_credentials)
         self.connection.get_warnings = True
-        self.create_db()
-        self.use_this_db()
-        self.create_tables()
+        if not self.this_db_exist():
+            self.create_db()
+            self.use_this_db()
+            self.create_tables()
+        else: 
+            self.use_this_db()
 
 
     def close_db(self):
         self.connection.close()
+
+
+    def this_db_exist(self):
+        return False
 
 
     def execute_a_query(self, command, params=(), multi=False):
@@ -45,6 +52,13 @@ class DBWrapper():
 
 
     def create_tables(self):
-        pass
+        create_tables_file = "telegram_ecommerce/database/create_tables.sql"
+        commands = get_sql_commands_from_a_file(create_tables_file)
+        cursor = self.connection.cursor()
+        for command in commands:
+            cursor.execute(command)
+        self.connection.commit()
+        cursor.close()
+
 
 
