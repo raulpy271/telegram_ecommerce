a5692bcfac9277a5ccf335c98077c3a295cefbb5
Raul <raulrufino0@gmail.com>
2020-10-20 08:24:40 -0300

    adding function to load sql file

--- telegram_ecommerce/utils/utils.py
+++ telegram_ecommerce/utils/utils.py
@@ -1,8 +1,21 @@
 from json import load
 
 
+def load_file(path):
+    with open(path, "r") as io_file:
+        return io_file.read()
+
+
 def load_json_file(path):
     with open(path, "r") as json_string:
         return load(json_string)
 
+def get_sql_commands_from_a_file(path, delimiter=";"):
+    sql = load_file(path)
+    commands = ( sql
+        .replace("\n", "")
+        .split(delimiter)
+        )
+    commands.pop()
+    return commands
 
