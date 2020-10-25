030ce3bc24be0be809e93d29832423bbc1f16fb9
Raul <raulrufino0@gmail.com>
2020-10-25 16:56:03 -0300

    adding text module to facilitate language exchage

---
+++ telegram_ecommerce/utils/text.py
@@ -0,0 +1,46 @@
+
+text_en = ({
+    "start" : 
+"""hello, I'm a bot that will help you buy products in this e-commerce. 
+type /show_categories to see categories.
+type /login to autenticate your account.
+type /help for more information.""",
+    "help" : 
+
+"""The fallowing are instructions of how use this bot:
+
+type /show_categories to see products by category.
+
+type /show_products to see products by a especified category.
+The syntax are:
+/show_products product name
+
+To search to something you can use the commands:
+/search_product and /search_category.
+The syntax are:
+/search_product product name
+/search_category category name
+And these commands will show you a list with the thing you want buy.
+
+To buy something you need to register a password. 
+To do this type /login.""",
+    "help_admin":
+"""
+
+You are admin. You can sell products and change theirs prices and add others admins.
+
+The fallowing are instructions of how make this things:
+
+To add a product you can type:
+/add_product
+To change the settings of a product you can search for it and for a button that does these actions.
+
+To add others admin you can use the command(Note: have sure that the people already have an account in this e-commerce):
+/add_admin username
+""",
+    "cancel" : "❌Cancel",
+    "previus" : "⬅️ Previus",
+    "next" : "➡️ Next",
+})
+
+text_pt = {}
