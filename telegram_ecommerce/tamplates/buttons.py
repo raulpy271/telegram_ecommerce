2d196d7073c341c4968c2cea5f659808bed9e27c
Raul <raulrufino0@gmail.com>
2020-10-26 09:02:07 -0300

    adding tamplates inside functions to putr identifier in callback data. with this, i can capture the callback query with a pattern that i want

--- telegram_ecommerce/tamplates/buttons.py
+++ telegram_ecommerce/tamplates/buttons.py
@@ -5,44 +5,48 @@ from telegram import (
 
 from ..utils.consts import TEXT
 
-numeric_keyboard = InlineKeyboardMarkup([
+def numeric_keyboard(pattern_identifier):
+    return (InlineKeyboardMarkup([
     [
-        Button("1", callback_data='1'),
-        Button("2", callback_data='2'),
-        Button("3", callback_data='3')
+        Button("1", callback_data=pattern_identifier + '1'),
+        Button("2", callback_data=pattern_identifier + '2'),
+        Button("3", callback_data=pattern_identifier + '3')
     ],
     [
-        Button("4", callback_data='4'),
-        Button("5", callback_data='5'),
-        Button("6", callback_data='6')
+        Button("4", callback_data=pattern_identifier + '4'),
+        Button("5", callback_data=pattern_identifier + '5'),
+        Button("6", callback_data=pattern_identifier + '6')
     ],
     [
-        Button("7", callback_data='7'),
-        Button("8", callback_data='8'),
-        Button("9", callback_data='9')
+        Button("7", callback_data=pattern_identifier + '7'),
+        Button("8", callback_data=pattern_identifier + '8'),
+        Button("9", callback_data=pattern_identifier + '9')
     ],
     [
         Button(TEXT["cancel"], 
-            callback_data='cancel_numeric_keyboard'),
-        Button("0", callback_data='0'),
+            callback_data=pattern_identifier + 'cancel_numeric_keyboard'),
+        Button("0", callback_data=pattern_identifier + '0'),
         Button(TEXT["next"], 
-            callback_data='end_numeric_keyboard')
-    ]])
+            callback_data=pattern_identifier + 'end_numeric_keyboard')
+    ]]))
 
 
-login_keyboard = {
+
+def login_keyboard(pattern_identifier): 
+    return ({
     "step_1": InlineKeyboardMarkup([
     [
-        Button(TEXT["cancel"], callback_data='cancel_loging_process'),
-        Button(TEXT["next"], callback_data='next_step_1_login_process'),
+        Button(TEXT["cancel"], 
+            callback_data=pattern_identifier + 'cancel_loging_process'),
+        Button(TEXT["next"], 
+            callback_data=pattern_identifier + 'next_step_1_login_process'),
     ]]),
-    "step_2": numeric_keyboard,
+    "step_2": numeric_keyboard(pattern_identifier),
     "step_3": InlineKeyboardMarkup([
     [
-        Button(TEXT["cancel"], callback_data='cancel_loging_process'),
-        Button(TEXT["next"], callback_data='end_login_process'),
-    ]])
-}
-
-
+        Button(TEXT["cancel"], 
+            callback_data=pattern_identifier + 'cancel_loging_process'),
+        Button(TEXT["next"],
+            callback_data=pattern_identifier + 'end_login_process'),
+    ]]) })
 
