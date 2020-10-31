8c3ed8bd2ca07307b8dec68e80aebb2bd7b3f690
Raul <raulrufino0@gmail.com>
2020-10-31 10:30:21 -0300

    adding function to create user in db

--- telegram_ecommerce/database/manipulation.py
+++ telegram_ecommerce/database/manipulation.py
@@ -4,6 +4,19 @@ from ..utils.utils import hash_password
 from ..utils.consts import credentials
 
 
+def create_account(user):
+    user_id = user.id
+    username = user.username
+    user_is_admin = user_in_credentials_file(username)
+    command = "UPDATE customers SET password_hash = %s WHERE user_id = %s"
+    command = ("""
+        INSERT INTO customers 
+            (user_id, username, password_hash, is_admin) 
+            VALUES (%s, %s, %s, %s)""")
+    command_args = (user_id, username, "", user_is_admin)
+    db.execute_a_data_manipulation(command, command_args)
+
+
 def set_password(user_id, password):
     command = "UPDATE customers SET password_hash = %s WHERE user_id = %s"
     db.execute_a_data_manipulation(command, (password, user_id))
