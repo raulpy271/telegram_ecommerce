23355eae8dd14885222c8954ebf3288f08bce4f6
Raul <raulrufino0@gmail.com>
2020-10-31 08:38:39 -0300

    adding function to test if user exist

--- telegram_ecommerce/database/query.py
+++ telegram_ecommerce/database/query.py
@@ -1,6 +1,13 @@
 from ..utils.utils import extract_value_from_a_query
 from .db_wrapper import db
 
+
+def user_exist(user_id):
+    command = "SELECT * FROM customers WHERE user_id = %s"
+    user_exist = bool(db.execute_a_query(command, (user_id,)))
+    return user_exist
+
+
 def is_admin(user_id):
     command = "SELECT is_admin FROM customers WHERE user_id = %s"
     user_is_admin = db.execute_a_query(command, (user_id,))
@@ -18,3 +25,5 @@ def get_password(user_id):
 
 def check_password(user_id, password):
     return str(password) == str(get_password(user_id))
+
+
