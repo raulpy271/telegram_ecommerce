7e5ab0c100762694e2dc28ecef684c36922328b7
Raul <raulrufino0@gmail.com>
2020-11-05 18:51:55 -0300

    fixing error in message tamplate

--- telegram_ecommerce/tamplates/messages.py
+++ telegram_ecommerce/tamplates/messages.py
@@ -1,3 +1,4 @@
+from ..utils.consts import TEXT
 from .buttons import boolean_question
 
 def reply(update, context, text):
@@ -9,16 +10,11 @@ def ask_a_boolean_question(
     context, 
     pattern_identifier="", 
     question=None):
-
-
     if question:
         text = question
     else:
         text = TEXT["ask_if_its_all_ok"]
-
-
-    user_id = query.from_user.id
     markup = boolean_question(pattern_identifier)
-    query.edit_message_text(text, reply_markup=markup)
+    update.message.reply_text(text, reply_markup=markup)
 
 
