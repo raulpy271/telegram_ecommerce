36a48ba062b2cbd4698f75d97e237535a640803a
Raul <raulrufino0@gmail.com>
2020-11-07 08:31:22 -0300

    adding function to create a a Keyboard from a list

--- telegram_ecommerce/tamplates/buttons.py
+++ telegram_ecommerce/tamplates/buttons.py
@@ -62,3 +62,15 @@ def login_keyboard(pattern_identifier):
     ]]) })
 
 
+def get_list_of_buttons(pattern_identifier, *names_in_buttons):
+    list_of_buttons = []
+    for name_of_the_button in names_in_buttons:
+        list_of_buttons.append(
+            [
+                Button(name_of_the_button,
+                    callback_data=pattern_identifier + name_of_the_button)
+            ]
+        )
+    return InlineKeyboardMarkup(list_of_buttons)
+
+
