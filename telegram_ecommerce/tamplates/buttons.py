3d38ff088d418fbf53cd9b089552cddae6b67cda
Raul <raulrufino0@gmail.com>
2020-11-08 14:48:49 -0300

    changing the return of the function that create buttons from a list. now is returning a ReplyKeyboardMarkup instead of a InlineMarup. With that, its not necessary to use an callback query

--- telegram_ecommerce/tamplates/buttons.py
+++ telegram_ecommerce/tamplates/buttons.py
@@ -1,6 +1,8 @@
 
 from telegram import (
-    InlineKeyboardButton as Button,
+    KeyboardButton as Button,
+    ReplyKeyboardMarkup,
+    InlineKeyboardButton as InlineButton,
     InlineKeyboardMarkup)
 
 from ..utils.consts import TEXT
@@ -9,9 +11,9 @@ from ..utils.consts import TEXT
 def boolean_question(pattern_identifier):
     return InlineKeyboardMarkup([
         [
-            Button(TEXT["cancel"], 
+            InlineButton(TEXT["cancel"], 
                 callback_data=pattern_identifier + 'cancel'),
-            Button(TEXT["OK"], 
+            InlineButton(TEXT["OK"], 
                 callback_data=pattern_identifier + 'OK')
         ]
     ])
@@ -20,25 +22,25 @@ def boolean_question(pattern_identifier):
 def numeric_keyboard(pattern_identifier):
     return (InlineKeyboardMarkup([
     [
-        Button("1", callback_data=pattern_identifier + 'digit_1'),
-        Button("2", callback_data=pattern_identifier + 'digit_2'),
-        Button("3", callback_data=pattern_identifier + 'digit_3')
+        InlineButton("1", callback_data=pattern_identifier + 'digit_1'),
+        InlineButton("2", callback_data=pattern_identifier + 'digit_2'),
+        InlineButton("3", callback_data=pattern_identifier + 'digit_3')
     ],
     [
-        Button("4", callback_data=pattern_identifier + 'digit_4'),
-        Button("5", callback_data=pattern_identifier + 'digit_5'),
-        Button("6", callback_data=pattern_identifier + 'digit_6')
+        InlineButton("4", callback_data=pattern_identifier + 'digit_4'),
+        InlineButton("5", callback_data=pattern_identifier + 'digit_5'),
+        InlineButton("6", callback_data=pattern_identifier + 'digit_6')
     ],
     [
-        Button("7", callback_data=pattern_identifier + 'digit_7'),
-        Button("8", callback_data=pattern_identifier + 'digit_8'),
-        Button("9", callback_data=pattern_identifier + 'digit_9')
+        InlineButton("7", callback_data=pattern_identifier + 'digit_7'),
+        InlineButton("8", callback_data=pattern_identifier + 'digit_8'),
+        InlineButton("9", callback_data=pattern_identifier + 'digit_9')
     ],
     [
-        Button(TEXT["cancel"], 
+        InlineButton(TEXT["cancel"], 
             callback_data=pattern_identifier + 'cancel_numeric_keyboard'),
-        Button("0", callback_data=pattern_identifier + 'digit_0'),
-        Button(TEXT["next"], 
+        InlineButton("0", callback_data=pattern_identifier + 'digit_0'),
+        InlineButton(TEXT["next"], 
             callback_data=pattern_identifier + 'end_numeric_keyboard')
     ]]))
 
@@ -47,17 +49,17 @@ def login_keyboard(pattern_identifier):
     return ({
     "step_1": InlineKeyboardMarkup([
     [
-        Button(TEXT["cancel"], 
+        InlineButton(TEXT["cancel"], 
             callback_data=pattern_identifier + 'cancel_loging_process'),
-        Button(TEXT["next"], 
+        InlineButton(TEXT["next"], 
             callback_data=pattern_identifier + 'next_step_1_login_process'),
     ]]),
     "step_2": numeric_keyboard(pattern_identifier),
     "step_3": InlineKeyboardMarkup([
     [
-        Button(TEXT["cancel"], 
+        InlineButton(TEXT["cancel"], 
             callback_data=pattern_identifier + 'cancel_loging_process'),
-        Button(TEXT["next"],
+        InlineButton(TEXT["next"],
             callback_data=pattern_identifier + 'end_login_process'),
     ]]) })
 
@@ -67,10 +69,9 @@ def get_list_of_buttons(pattern_identifier, *names_in_buttons):
     for name_of_the_button in names_in_buttons:
         list_of_buttons.append(
             [
-                Button(name_of_the_button,
-                    callback_data=pattern_identifier + name_of_the_button)
+                Button(name_of_the_button)
             ]
         )
-    return InlineKeyboardMarkup(list_of_buttons)
+    return ReplyKeyboardMarkup(list_of_buttons)
 
 
