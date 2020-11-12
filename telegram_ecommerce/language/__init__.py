7a627f48ac41139f7a790cfaf914be97f7713f05
Raul <raulrufino0@gmail.com>
2020-11-12 15:45:10 -0300

    putting the funtion get_text in the Language class

--- telegram_ecommerce/language/__init__.py
+++ telegram_ecommerce/language/__init__.py
@@ -1,7 +1,6 @@
 from .text_en import text_en
 from .text_pt import text_pt
 from ..utils.consts import default_language
-from ..utils.utils import get_lang
 
 
 all_languages = {
@@ -10,6 +9,12 @@ all_languages = {
 
 
 class Language():
+    """this module imports all the text that is shown to the user.
+The get_text method receives the key that contains the 
+text that will be shown to the user. This method also receives 
+the context which indicates the user's language."""
+
+
     def __init__(self, dict_with_all_languages, language):
         self.default_language = language
         self.suported_languages = dict_with_all_languages.keys()
@@ -17,11 +22,26 @@ class Language():
 
 
     def get_text(self, key, context=None):
-        language = get_lang(context)
-        if language in self.suported_languages:
-            return self.all_text[language][key]
+        """ This method return the text, the langauge are passed in the context argument.
+        >>> TEXT.get_text('OK', 'en')
+        'OK'
+        >>> TEXT.get_text('OK', 'pt')
+        'Certo'
+        """
+        language = self.extract_lang(context)
+        return self.all_text[language][key]
+
+
+    def extract_lang(self, context):
+        if hasattr(context, 'user_data'):
+            user_data = context.user_data
+            if "language" in user_data:
+                return self.get_lang(user_data["language"])
+            return self.default_language
+        elif context in self.suported_languages:
+            return context
         else:
-            return self.all_text[self.default_language][key]
+            return self.default_language
 
 
 TEXT = Language(all_languages, default_language)
