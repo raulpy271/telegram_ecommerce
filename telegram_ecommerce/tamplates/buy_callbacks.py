2c064c02ab012c1892686e6323dab35b26ada785
Raul <raulrufino0@gmail.com>
2020-11-19 17:08:09 -0300

    importing the currency and provider_token in callback send_a_shipping_message

--- telegram_ecommerce/tamplates/buy_callbacks.py
+++ telegram_ecommerce/tamplates/buy_callbacks.py
@@ -1,4 +1,8 @@
+from telegram import LabeledPrice
 
+from ..utils.consts import (
+    provider_token,
+    currency)
 from ..tamplates.products import (
     send_a_detailed_product)
 
@@ -8,6 +12,26 @@ def process_of_buy_a_product(update, context, product, pattern_identifier):
 
 
 def send_a_shipping_message(update, context, product , pattern_identifier):
-    pass
+    title = product.name
+    description = product.description
+    payload = "Custom-Payload"
+    start_parameter = "test-payment"
+    prices = [LabeledPrice("Price", product.price)]
+
+
+    context.bot.send_invoice(
+        update.message.chat_id, 
+        title, 
+        description, 
+        payload, 
+        provider_token, 
+        start_parameter, 
+        currency,
+        prices,
+        need_name = True,
+        need_phone_number = True, 
+        need_email = True, 
+        need_shipping_address = True
+    )
 
 
