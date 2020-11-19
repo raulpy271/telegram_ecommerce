b7fcc59e73f6f1c49300300e4d4479644954c98f
Raul <raulrufino0@gmail.com>
2020-11-19 17:05:08 -0300

    adding provider token and currency in consts file

--- telegram_ecommerce/utils/consts.py
+++ telegram_ecommerce/utils/consts.py
@@ -1,7 +1,10 @@
 from .utils import load_json_file
 
 default_language = "en"
+currency = "USD"
 credentials_path = "telegram_ecommerce/utils/.user_credentials.json"
 credentials = load_json_file(credentials_path)
 db_credentials = credentials["db_credentials"]
+provider_token = credentials["provider_token"]
+
 
