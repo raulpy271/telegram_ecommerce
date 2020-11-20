a52f93c0106e6cb3f69488fa34afef18d17c2d66
Raul <raulrufino0@gmail.com>
2020-11-20 17:47:14 -0300

    updating the text that appear in the help command

--- telegram_ecommerce/language/text_en.py
+++ telegram_ecommerce/language/text_en.py
@@ -3,7 +3,7 @@ text_en = ({
     "start" : 
 """hello, I'm a bot that will help you buy products in this e-commerce. 
 type /show_categories to see categories.
-type /login to autenticate your account.
+type /register to autenticate your account.
 type /help for more information.""",
     "start_description" : "A wellcome message if you dont known this bot",
     "help" : 
@@ -12,16 +12,7 @@ type /help for more information.""",
 
 type /show_categories to see products by category.
 
-type /show_products to see products by a especified category.
-The syntax are:
-/show_products product name
-
-To search to something you can use the commands:
-/search_product and /search_category.
-The syntax are:
-/search_product product name
-/search_category category name
-And these commands will show you a list with the thing you want buy.
+To search to something you can use the command /search
 
 To buy something you need to register a password. 
 To do this type /login.""",
@@ -78,6 +69,7 @@ To add others admin you can use the command(Note: have sure that the people alre
     "ask_for_category_tags" : "Type the tags of the category:",
     "ask_for_category_photo" : "Send me a photo that represent this category:",
     "ask_for_product_name" : "Type the name of the product:",
+    "ask_for_product_description" : "Type the description of the product:",
     "ask_for_product_price" : "Type the price of the product:",
     "ask_for_quantity_in_stock" : "Type the number of units of this product available:",
     "ask_for_category_name_of_the_product" : "Type the name of the category which this product is in:",
