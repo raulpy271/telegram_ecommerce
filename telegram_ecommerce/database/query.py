f6e05d5115f5639746eca6993eaa67f127fc20e3
Raul <raulrufino0@gmail.com>
2020-11-23 12:01:38 -0300

    altering query that return all products by name. now this query return only available products

--- telegram_ecommerce/database/query.py
+++ telegram_ecommerce/database/query.py
@@ -64,16 +64,17 @@ def get_category_id_from_name(name):
     return extract_value_from_a_query(category_id)
 
 
-def get_products_by_category_id(category_id):
-    command = "SELECT * FROM products WHERE category_id = %s"
+def get_all_available_by_category_id(category_id):
+    command = """ SELECT * FROM products 
+        WHERE category_id = %s AND quantity_in_stock > 0"""
     products_with_category_id = db.execute_a_query(
         command, (category_id,))
     return products_with_category_id
 
 
-def get_products_by_category_name(name):
+def get_all_available_by_category_name(name):
     category_id = get_category_id_from_name(name)
-    return get_products_by_category_id(category_id)
+    return get_all_available_by_category_id(category_id)
 
 
 def get_quantity_in_stock(product_id):
