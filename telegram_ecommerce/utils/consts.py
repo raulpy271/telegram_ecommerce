7f206b1fac8e8691ac96b256124dfc83bf518213
Raul <raulrufino0@gmail.com>
2020-11-25 11:44:07 -0300

    adding consts in to represent the values: "good", "regular" and "bad"

--- telegram_ecommerce/utils/consts.py
+++ telegram_ecommerce/utils/consts.py
@@ -6,5 +6,8 @@ credentials_path = "telegram_ecommerce/utils/.user_credentials.json"
 credentials = load_json_file(credentials_path)
 db_credentials = credentials["db_credentials"]
 provider_token = credentials["provider_token"]
+BAD_RATING = 0
+REGULAR_RATING = 5
+GOOD_RATING = 10
 
 
