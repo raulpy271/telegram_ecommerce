f6644dd3dba7b13732d7d8449c347f49ce696bcb
Raul <raulrufino0@gmail.com>
2020-12-11 18:02:30 -0300

    changing names of columns in database

--- telegram_ecommerce/database/create_tables.sql
+++ telegram_ecommerce/database/create_tables.sql
@@ -1,7 +1,7 @@
 
 
 CREATE TABLE IF NOT EXISTS customers (
-    user_id INT PRIMARY KEY,
+    id INT PRIMARY KEY,
     username VARCHAR(30) NOT NULL,
     password_hash VARCHAR(64),
     is_admin BOOLEAN NOT NULL DEFAULT FALSE
@@ -9,50 +9,50 @@ CREATE TABLE IF NOT EXISTS customers (
 
 
 CREATE TABLE IF NOT EXISTS photo (
-    photo_id VARCHAR(150) PRIMARY KEY,
-    image BLOB DEFAULT NULL
+    id VARCHAR(150) PRIMARY KEY,
+    image_blob BLOB DEFAULT NULL
 ) engine=InnoDB;
 
 
 CREATE TABLE IF NOT EXISTS category (
-    category_id INT AUTO_INCREMENT PRIMARY KEY,
-    category_name VARCHAR(30) NOT NULL,
-    category_description VARCHAR(500) NOT NULL,
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(30) NOT NULL,
+    description VARCHAR(500) NOT NULL,
     tags VARCHAR(100),
     image_id VARCHAR(150),
     FOREIGN KEY (image_id)
-    REFERENCES photo (photo_id)
+    REFERENCES photo (id)
 ) engine=InnoDB;
 
 
 CREATE TABLE IF NOT EXISTS products (
-    product_id INT AUTO_INCREMENT PRIMARY KEY, 
+    id INT AUTO_INCREMENT PRIMARY KEY, 
     name VARCHAR(30) NOT NULL,
-    product_description VARCHAR(500) NOT NULL,
-    unit_price FLOAT NOT NULL,
+    description VARCHAR(500) NOT NULL,
+    price FLOAT NOT NULL,
     quantity_in_stock INT NOT NULL,
     quantity_purchased INT NOT NULL,
     category_id INT NOT NULL,
     image_id VARCHAR(150),
     FOREIGN KEY (image_id)
-    REFERENCES photo (photo_id),
+    REFERENCES photo (id),
     FOREIGN KEY (category_id)
-    REFERENCES category (category_id)
+    REFERENCES category (id)
 ) engine=InnoDB;
 
 
 CREATE TABLE IF NOT EXISTS orders (
-    order_id VARCHAR(150) PRIMARY KEY,
+    id VARCHAR(150) PRIMARY KEY,
     price FLOAT NOT NULL,
     user_id INT NOT NULL,
     product_id INT NOT NULL,
     rating INT,
     FOREIGN KEY (user_id)
-    REFERENCES customers (user_id)
+    REFERENCES customers (id)
         ON UPDATE RESTRICT
         ON DELETE RESTRICT,
     FOREIGN KEY (product_id)
-    REFERENCES products (product_id)
+    REFERENCES products (id)
         ON UPDATE RESTRICT
         ON DELETE RESTRICT
 ) engine=InnoDB;
